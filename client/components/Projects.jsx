43efa99eb5fcc43ac3c191d599153bf06c003e70
Brian Price <brianprice88@gmail.com>
2020-05-17 15:47:57 -0400

    "set links to videos and source code.  need to format images properly and add descriptions of what i did for each project"

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -4,67 +4,75 @@ export default function Projects() {
     return (
         <div className="main">
 
-        <h1>Projects</h1>
-        <br />
-        
-        <h2>PORTFOLIO</h2>
-        <p>Resize the browser window to see the responsive effect.</p>
-
-        <div class="content">
-          <video src="assets/RockPaperScissors.mov" controls alt="Trafalgar"/>
-          <h3>Some Other Work</h3>
-          <p>Lorem ipsum..</p>
+        <div className="content">
+        <h3>Rock Paper Scissors online multiplayer game</h3>
+          <p>Technologies used: <strong>React, Socket.IO, Express</strong></p>
+          <p>Play game <a href='https://rock-paper-scissors-brianprice.herokuapp.com/' target="_blank">here!</a></p>
+          <p>Or view the source code <a href='https://github.com/brianprice88/Rock-Paper-Scissors' target="_blank">here</a></p>
+          <video src="assets/RockPaperScissors.mov" autoPlay loop muted alt="Rock Paper Scissors"/>
         </div>
 
-        <div class="content">
-          <video src="assets/Minesweeper.mov" controls alt="Trafalgar"/>
-          <h3>Some Other Work</h3>
-          <p>Lorem ipsum..</p>
+        <div className="content">
+        <h3>Minesweeper online game</h3>
+          <p>Technologies used: <strong>HTML, CSS, JavaScript, MongoDB</strong></p>
+          <p>Play game <a href='https://minesweeper-brian-price.herokuapp.com/' target="_blank">here!</a></p>
+          <p>Or view the source code <a href='https://github.com/brianprice88/Minesweeper' target="_blank">here</a></p>
+          <video src="assets/Minesweeper.mov" autoPlay loop muted alt="Minesweeper demo"/>
         </div>
         
-        <div class="row">
-
-          <div class="column">
-            <div class="content">
-              <img src='assets/Geofencing.png' alt="Mountains"/>
-              <h3>My Work</h3>
-              <p>Lorem ipsum..</p>
+        <div className="row">
+            <h3>Geofencing-notification mobile app</h3>
+            <p>Technologies used: <strong>React Native, GraphQ, and MongoDB</strong></p>
+            <p>View the source code <a href='https://github.com/hrla34-MVP/Server-database' target="_blank">here</a></p>
+          <div className="column">
+            <div className="content">
+              <img src='assets/Geofencing.png' alt="Geofencing app image"/>
             </div>
           </div>
-          <div class="column">
-            <div class="content">
-              <img src='assets/Geofencing2.png' alt="Lights"/>
-              <h3>My Work</h3>
-              <p>Lorem ipsum..</p>
+          <div className="column">
+            <div className="content">
+              <img src='assets/Geofencing2.png' alt="Geofencing app image"/>
             </div>
           </div>
-          <div class="column">
-            <div class="content">
-              <img src="assets/SDC.png" alt="Nature"/>
-              <h3>My Work</h3>
-              <p>Lorem ipsum..</p>
-            </div>
-          </div>
-          <div class="column">
-            <div class="content">
-              <img src="assets/SDC2.png" alt="Mountains"/>
-              <h3>My Work</h3>
-              <p>Lorem ipsum..</p>
-            </div>
+                
           </div>
 
-        </div>
-        
-        <div class="content">
-          <img src="assets/SDC3.png" alt="Bear"/>
+          <div className='row'>
+
+          <h3>REI Product View</h3>
+          <p>Technologies used: <strong>Express, PostgresSQL, MongoDB</strong></p>
+          <p>View the source code <a href='https://github.com/The-Good-Place-HRLA/Product-View-service' target="_blank">here</a></p>
+
+          <div className="column">
+            <div className="content">
+              <img src="assets/SDC.png" alt="REI Product View testing image"/>
+              <h3>My Work</h3>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <img src="assets/SDC2.png" alt="REI Product View testing image"/>
+              <h3>My Work</h3>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div>
+        <div className='column'>
+        <div className="content">
+          <img src="assets/SDC3.png" alt="REI Product View testing image"/>
           <h3>Some Other Work</h3>
           <p>Lorem ipsum..</p>
         </div>
+        </div>
 
-        <div class="content">
-          <video src="assets/Trafalgar.mp4" controls alt="Trafalgar"/>
-          <h3>Some Other Work</h3>
-          <p>Lorem ipsum..</p>
+        </div>
+
+
+        <div className="content">
+        <h3>Trafalgar trip page</h3>
+          <p>Technologies used: <strong>React, Express, MongoDB</strong></p>
+          <p>View the source code <a href='https://github.com/Front-End-Capstone-hrla34/Searchbar' target="_blank">here</a></p>
+          <video src="assets/Trafalgar.mp4" autoPlay loop muted alt="Trafalgar trip page demo"/>
         </div>
         
         </div>
