3c84cc5a58de73b0205b4f61918bbcdefe757375
Brian Price <brianprice88@gmail.com>
2020-05-18 15:40:36 -0400

    "add resume"

--- client/components/site.jsx
+++ client/components/site.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ContactForm from './ContactForm.jsx';
-import Projects from './Projects.jsx'
+import Projects from './Projects.jsx';
 
 export default function Site () {
     return (
     <div>
         <div id='Projects'>
         < Projects />
+        </div>
+
+        <div id='Resume'>
+        <iframe id='displayResume' src='assets/BrianPriceResume.pdf'></iframe>
         </div>
 
         <div id='ContactForm'>
