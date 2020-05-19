fc60e4973527e3a5caa33fd3763651772e70753f
Brian Price <brianprice88@gmail.com>
2020-05-19 03:09:00 -0400

    "get topbar correct but need to either make it fixed to page or figure out how to do it as a sidebar instead"

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
 export default function Navbar() {
+    
+    function toggleNavbar () {
+      let navBar = document.getElementById('myTopnav');
+      if (navBar.className === "topnav") {
+        navBar.className += " responsive";
+      } else {
+        navBar.className = "topnav";
+      }
+    }
+
     return (
-        <div className = 'navHeader'>
+        <div className = 'topnav' id='myTopnav'>
         <a href='#About'>About</a>
         <a href='#Projects'>Projects</a>
         <a href='#Resume'>Resume</a>
-        <a href='#ContactForm'>Contact</a>        
+        <a href='#ContactForm'>Contact</a>
+        <a className='icon' onClick = {toggleNavbar}>&#9776;</a>      
         </div>
     )
 }
