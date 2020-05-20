e6039d754b369e0a2089d7a9367564acf8826db8
Brian Price <brianprice88@gmail.com>
2020-05-20 02:32:41 -0400

    "start working on styling improvements"

--- client/components/site.jsx
+++ client/components/site.jsx
@@ -5,27 +5,19 @@ import About from './About.jsx';
 import Navbar from './Navbar.jsx';
 import Resume from './Resume.jsx';
 import Error from './Error.jsx';
-import {Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-export default function Site () {
+export default function Site() {
     return (
-    <div>
-
-        <Navbar />
-        <Switch>
-
-        <Route path='/' exact component={About} />
-
-        <Route path='/Projects' component={Projects} />
-
-        <Route path='/Resume' component={Resume} />
-
-        <Route path='/Contact' component={ContactForm} />
-
-        <Route component={Error} />
-
-        </Switch>
-
-    </div>
+        <div>
+            <Navbar />
+            <Switch>
+                <Route path='/' exact component={About} />
+                <Route path='/Projects' component={Projects} />
+                <Route path='/Resume' component={Resume} />
+                <Route path='/Contact' component={ContactForm} />
+                <Route component={Error} />
+            </Switch>
+        </div>
     )
 }
