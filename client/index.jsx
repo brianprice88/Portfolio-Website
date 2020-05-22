4eedf08b7e80f3291c7d3ed74e34f307f904510d
Brian Price <brianprice88@gmail.com>
2020-05-21 22:17:20 -0400

    "Heroku commmit"

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Site from './components/Site.jsx'
+import Site from './components/site.jsx'
 
 ReactDOM.render(
     <BrowserRouter>
