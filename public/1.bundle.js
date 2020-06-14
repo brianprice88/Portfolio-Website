e116691e83f9490ea035a7c04965038c067cb451
Brian Price <brianprice88@gmail.com>
2020-06-14 13:28:34 -0400

    "redeploy"

---
+++ public/1.bundle.js
@@ -0,0 +1,16 @@
+webpackJsonp([1],{
+
+/***/ 44:
+/* no static exports found */
+/* all exports used */
+/*!**************************************!*\
+  !*** ./client/components/Resume.jsx ***!
+  \**************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n    value: true\n});\nexports.default = Resume;\n\nvar _react = __webpack_require__(/*! react */ 1);\n\nvar _react2 = _interopRequireDefault(_react);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\nfunction Resume() {\n    return _react2.default.createElement(\n        'div',\n        { id: 'Resume' },\n        _react2.default.createElement('iframe', { id: 'displayResume', src: 'assets/BrianPriceResume.pdf' })\n    );\n}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNDQuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vY2xpZW50L2NvbXBvbmVudHMvUmVzdW1lLmpzeD85Y2EwIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCc7XG5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIFJlc3VtZSgpIHtcbiAgICByZXR1cm4gKFxuICAgICAgICA8ZGl2IGlkPSdSZXN1bWUnPlxuICAgICAgICAgICAgPGlmcmFtZSBpZD0nZGlzcGxheVJlc3VtZScgc3JjPSdhc3NldHMvQnJpYW5QcmljZVJlc3VtZS5wZGYnPjwvaWZyYW1lPlxuICAgICAgICA8L2Rpdj5cbiAgICApXG59XG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIGNsaWVudC9jb21wb25lbnRzL1Jlc3VtZS5qc3giXSwibWFwcGluZ3MiOiI7Ozs7O0FBRUE7QUFDQTtBQUhBO0FBQ0E7Ozs7O0FBQ0E7QUFDQTtBQUNBO0FBQUE7QUFDQTtBQURBO0FBSUEiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///44\n");
+
+/***/ })
+
+});
